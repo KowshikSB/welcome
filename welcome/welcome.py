263da3cf6c84384cca7712ea0a2f98c820cb2f57
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-04-20 10:45:22 +0530

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -22,10 +22,25 @@ class welcome(commands.Cog):
           
           em=discord.Embed(title="Cloudy With A Chance of Depression",description=x,color=0x2f3136)
           em.set_author(name=f'{member.name}#{member.discriminator}',icon_url=member.avatar_url)
-          em.set_thumbnail(url="https://cdn.discordapp.com/attachments/799598027257937921/829726047883886622/tenor_3.gif") 
+          em.set_thumbnail(url="https://imgur.com/QQcLvbH") 
           guild=self.bot.get_guild(799526257506254868)
           channel = guild.get_channel(827008183692296222) 
           
           await channel.send(f'<a:animesip:823846730888904724> {member.mention} <@&802797577300213790>',embed=em)
+    @commands.command()
+    async def join(self,ctx):
+        if ctx.author.id in [261742964441612298,533696842613915658]:
+          x='''<a:kawaiiStarryCloud:824531125678243891> <#800095824310566922> 
+<a:kawaiiStarryCloud:824531125678243891> <#800116012954550292>
+<a:kawaiiStarryCloud:824531125678243891> <#827008183692296222>'''
+          
+          em=discord.Embed(title="Cloudy With A Chance of Depression",description=x,color=0x2f3136)
+          em.set_author(name=f'{ctx.author.name}#{ctx.author.discriminator}',icon_url=ctx.author.avatar_url)
+          em.set_thumbnail(url="https://imgur.com/QQcLvbH") 
+          guild=self.bot.get_guild(799526257506254868)
+          channel = guild.get_channel(827008183692296222) 
+        else:
+            await ctx.message.react('<:cross:833933468487385099>')
+
 def setup(bot):
     bot.add_cog(welcome(bot))
