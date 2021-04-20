a66eed3cfaff7d82a197a2882b75b1ae87b20d14
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-04-20 10:53:20 +0530

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -28,18 +28,18 @@ class welcome(commands.Cog):
           
           await channel.send(f'<a:animesip:823846730888904724> {member.mention} <@&802797577300213790>',embed=em)
     @commands.command()
-    async def join(self,ctx):
-        if ctx.author.id in [261742964441612298,533696842613915658]:
+    async def join(self,message):
+        if message.author.id in [261742964441612298,533696842613915658]:
              x='''<a:kawaiiStarryCloud:824531125678243891> <#800095824310566922> 
 <a:kawaiiStarryCloud:824531125678243891> <#800116012954550292>
 <a:kawaiiStarryCloud:824531125678243891> <#827008183692296222>'''
           
              em=discord.Embed(title="Cloudy With A Chance of Depression",description=x,color=0x2f3136)
-             em.set_author(name=f'{ctx.author.name}#{ctx.author.discriminator}',icon_url=ctx.author.avatar_url)
+             em.set_author(name=f'{message.author.name}#{message.author.discriminator}',icon_url=message.author.avatar_url)
              em.set_thumbnail(url="https://imgur.com/QQcLvbH") 
              guild=self.bot.get_guild(799526257506254868)
              channel = guild.get_channel(827008183692296222) 
-             await channel.send(f'<a:animesip:823846730888904724> {ctx.author.mention} <@&802797577300213790>',embed=em)
+             await channel.send(f'<a:animesip:823846730888904724> {message.author.mention} <@&802797577300213790>',embed=em)
         
 def setup(bot):
     bot.add_cog(welcome(bot))
