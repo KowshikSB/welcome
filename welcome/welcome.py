8ee937a0604e1a66bbc43ebfbf2fc5fd663e1da4
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-06-01 10:49:32 +0530

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -16,9 +16,7 @@ class welcome(commands.Cog):
           
         if member.guild.id==799526257506254868:
       
-            x='''<a:kawaiiStarryCloud:824531125678243891> <#800095824310566922> 
-<a:kawaiiStarryCloud:824531125678243891> <#800116012954550292>
-<a:kawaiiStarryCloud:824531125678243891> <#800116101580193832>'''
+            x='''<a:capoopoof:847413169995513916>  <#800095824310566922>  <#800116012954550292> <#800116101580193832>'''
             
             em=discord.Embed(title="Cloudy With A Chance of Depression",description=x,color=0x2f3136)
             em.set_author(name=f'{member.name}#{member.discriminator}',icon_url=member.avatar_url)
@@ -32,9 +30,7 @@ class welcome(commands.Cog):
     async def testjoin(self,message):
         
         if message.author.id in [261742964441612298,533696842613915658]:
-             x='''<a:kawaiiStarryCloud:824531125678243891> <#800095824310566922> 
-<a:kawaiiStarryCloud:824531125678243891> <#800116012954550292>
-<a:kawaiiStarryCloud:824531125678243891> <#800116101580193832>'''
+             x='''<a:capoopoof:847413169995513916>  <#800095824310566922>  <#800116012954550292> <#800116101580193832>'''
           
              e=discord.Embed(title="Cloudy With A Chance of Depression",description=x,color=0x2f3136)
              e.set_author(name=f'{message.author.name}#{message.author.discriminator}',icon_url=message.author.avatar_url)
